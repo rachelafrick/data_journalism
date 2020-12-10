e5f47715a80e2f503ff39c78b904a573d7d98d56
Rachel Africk <rachel.africk21@trinityschoolnyc.org>
2020-12-10 15:00:05 -0500

    add first two tests to data.test.js

--- test/data.test.js
+++ test/data.test.js
@@ -1,14 +1,40 @@
-const data = require('data/coverage.json');
-
+const data = require('../data/coverage.json');
+//1. check that percentages are formatted as decimals
 test('all percents should be decimals', () => {
   let decimal = false
-  if (data[1].medicaid <=1 ) decimal = true;
+  for (let i = 0; i< data.length; i++){
+  if (data[i].medicaid <=1 && data[i].medicare <=1 &&data[i].employer <=1&&data[i].uninsured <=1&&data[i].military <=1 &&data[i].nongroup <=1) decimal = true;
+  else {
+    decimal = false;
+    break;
+  }
+  }
   expect(decimal).toBe(true);
 });
 
 
-/*1. check that percentages are formatted as decimals
-2. check for zeroes/missing data
+
+//2. check for zeroes/missing data
+
+test('np empty data points', () => {
+  let empty = false;
+  for (let i = 0; i< data.length; i++){
+
+  if (data[i].medicaid != "" && data[i].medicare !="" &&data[i].employer !="" &&data[i].uninsured  !="" &&data[i].military !="" &&data[i].nongroup !=""){
+     empty = true;
+   }
+   else {
+     empty = false
+     break;
+   }
+
+ }
+   expect(empty).toBe(true);
+});
+
+
+
+/*
 3. check for 51 (states + national average)
 4. check that each has seven labels (medicate, medicaid, etc)
 5. check that no one category is larger than 1 for a single state
@@ -16,5 +42,5 @@ test('all percents should be decimals', () => {
 7. check that there are no duplicate states
 8. check that the national average is the average of the data from the 50 states
 9. US Average should be labeled United States
-10. check that data is in alphabetical order by state 
+10. check that data is in alphabetical order by state
 */
