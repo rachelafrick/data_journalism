2645f29063136948f8617d691b6c90248dfa274a
Rachel Africk <rachel.africk21@trinityschoolnyc.org>
2020-12-13 19:46:51 -0500

    adding tests 8 and 9

--- test/data.test.js
+++ test/data.test.js
@@ -16,7 +16,7 @@ test('all percents should be decimals', () => {
 
 //2. check for zeroes/missing data
 
-test('np empty data points', () => {
+test('no empty data points', () => {
   let empty = false;
   for (let i = 0; i< data.length; i++){
 
@@ -32,6 +32,50 @@ test('np empty data points', () => {
    expect(empty).toBe(true);
 });
 
+//8. check that the national average is the average of the data from the 50 states
+
+test('average is true average', () => {
+  let average = 0;
+  for (let i = 1; i< data.length; i++){
+ average+=data[i].unisured;
+
+ }
+ average = (average/50)
+   expect(average).toBe(data[0].unisured);
+});
+
+//9. US Average should be labeled United States
+test('average is first and labeled United States', () => {
+  let avgLabel = false;
+
+  if (data[0].location == "United States"){
+     avgLabel = true;
+   }
+   else {
+     avgLabel = false
+     break;
+   }
+
+ }
+   expect(avgLabel).toBe(true);
+});
+
+//10. check that the data is in alphabetical order by state
+test('average is first and labeled United States', () => {
+  let avgLabel = false;
+
+  if (data[0].location == "United States"){
+     avgLabel = true;
+   }
+   else {
+     avgLabel = false
+     break;
+   }
+
+ }
+   expect(avgLabel).toBe(true);
+});
+
 
 
 /*
@@ -39,6 +83,7 @@ test('np empty data points', () => {
 4. check that each has seven labels (medicate, medicaid, etc)
 5. check that no one category is larger than 1 for a single state
 6. check that each states totals for the types add up to 1
+
 7. check that there are no duplicate states
 8. check that the national average is the average of the data from the 50 states
 9. US Average should be labeled United States
