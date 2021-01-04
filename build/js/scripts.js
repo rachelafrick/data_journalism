a1aad6ed892e037b96dea72393198e14056071eb
sanyaarora <44238444+sanyaarora@users.noreply.github.com>
2021-01-04 18:43:36 -0500

    testing example

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,6 +1,6 @@
 /*console.log("js/scripts.js connected to index.html");
 localStorage.setItem("top-secret-information", "you would never guess what kind of data is stored here."); */
-
+/*
 let data1= [
   {
     "location": "United States",
@@ -463,10 +463,7 @@ let data1= [
   }
 ]
 
-Plotly.d3.csv('./data/coveragedata.csv', function(err, rows){
-      function unpack(rows, key) {
-          return rows.map(function(row) { return row[key]; });
-      }
+
 
       var amtuninsured =[];
       var locs=[];
@@ -517,6 +514,52 @@ Plotly.d3.csv('./data/coveragedata.csv', function(err, rows){
           }
       };
 
-      Plotly.newPlot("myDiv", data2, layout, {showLink: false});
+      Plotly.newPlot("myDiv", data2, layout, {showLink: false}); */
+
+
+      Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+      var data = [{
+          type: 'choropleth',
+          locationmode: 'USA-states',
+          locations: unpack(rows, 'code'),
+          z: unpack(rows, 'total exports'),
+          text: unpack(rows, 'state'),
+          zmin: 0,
+          zmax: 17000,
+          colorscale: [
+              [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
+              [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
+              [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
+          ],
+          colorbar: {
+              title: 'Millions USD',
+              thickness: 0.2
+          },
+          marker: {
+              line:{
+                  color: 'rgb(255,255,255)',
+                  width: 2
+              }
+          }
+      }];
+
+
+      var layout = {
+          title: '2011 US Agriculture Exports by State',
+          geo:{
+              scope: 'usa',
+              showlakes: true,
+              lakecolor: 'rgb(255,255,255)'
+          }
+      };
+
+      Plotly.newPlot("myDiv", data, layout, {showLink: false});
 });
+
+
+
 
