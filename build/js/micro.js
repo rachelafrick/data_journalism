4df42b22c612bf2cb83e24b53bfe3b67882aefca
sanyaarora <44238444+sanyaarora@users.noreply.github.com>
2021-01-06 14:43:04 -0500

    error

--- build/js/micro.js
+++ build/js/micro.js
@@ -3,12 +3,11 @@
 
 var names= document.getElementById("namee").textContent;
 var indexx=0;
-var mdata=[];
+let mdata=[];
 console.log(names);
 
 Plotly.d3.json('./data/coverage.json', function(err, fig) {
 	mdata=fig;
-	console.log(mdata);
 	for(let i=1; i<fig.length;i++){
 		names=names.replace(" ","")
 		let string = fig[i].location.replace(" ", "")
@@ -42,7 +41,7 @@ Plotly.d3.json('./data/coverage.json', function(err, fig) {
 	 Plotly.newPlot(fig[indexx].location.replace(" ", ""), bars, layout);
 
 });
-
+	console.log(mdata);
 	if(mdata[indexx].uninsured<mdata[0].uninsured){
 		document.getElementbyId("analysis").innerText="The data for " + mdata[indexx].location + " shows that the majority of people in " + mdata[indexx].location + " recieve insurance through their employer. " + mdata[indexx].uninsured + " percent of the population of " + mdata[indexx].location + " is uninsured. This number is less then the national average, meaning " + mdata[indexx].location + " has a higher health care coverage rate than the U.S. as a whole." ;
 	}
