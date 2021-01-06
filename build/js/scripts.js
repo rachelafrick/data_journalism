50f38d41cea43822cf0143383d44a43a735f10ce
Rachel Africk <rachel.africk21@trinityschoolnyc.org>
2021-01-06 09:38:39 -0500

    CORS

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,6 +1,6 @@
 /*console.log("js/scripts.js connected to index.html");
 localStorage.setItem("top-secret-information", "you would never guess what kind of data is stored here."); */
-
+/*
 let data1= [
   {
     "location": "United States",
@@ -463,12 +463,15 @@ let data1= [
   }
 ]
 
-
-
+*/
+const fs = require("fs")
+let data1 = fs.readFileSync("./data/coverage.json")
+data1 = JSON.parse(data1);
+console.log(data1)
       var amtuninsured =[];
       var locs=[];
       var index=0;
- 
+
 
 
       for(var i =1; i<51;i++){
@@ -523,7 +526,7 @@ let data1= [
       Plotly.newPlot("myDiv", data2, layout).then(gd => {
 		  gd.on('plotly_click', d => {
 		    var pt = (d.points || [])[0]
-		    
+
 		    /*switch(pt.location) {
 		      case 'CAN':
 		        console.log('you clicked on CAN')
@@ -541,14 +544,14 @@ let data1= [
 		    }
 
 		    window.location.assign("https://rachelafrick.github.io/data_journalism/build/" + data1[index+1].location.trim().replace(/ /g, "_")+".html")
-		    
+
 		  })
-		}) 
+		})
 
-  }); 
+  });
 
 
-      
+
 
 
 /*
@@ -596,10 +599,6 @@ let data1= [
       };
 
       Plotly.newPlot("myDiv", data, layout, {showLink: false});
-}); 
+});
 
 */
-
-
-
-
