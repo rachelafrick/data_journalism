b991a350ed5fc258eec912577c5fdc8633f7d000
sanyaarora <44238444+sanyaarora@users.noreply.github.com>
2021-01-06 10:07:25 -0500

    linked plot to json

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -479,10 +479,9 @@ let data1= [
       let locas= [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',  'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
 
-      Plotly.d3.csv('./data/coveragedata.csv', function(err, rows){
-      function unpack(rows, key) {
-          return rows.map(function(row) { return row[key]; });
-      }
+      Plotly.d3.json('./data/coverage.json', function(err, fig){
+
+        console.log(fig);
 
 
       var data2 = [{
