31aa9738b246eb58d7e73be2b09fe268b523189f
sanyaarora <44238444+sanyaarora@users.noreply.github.com>
2021-01-06 19:30:04 -0500

    colors

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -28,11 +28,7 @@
           text: locs,
           zmin: 0,
           zmax: 20,
-          colorscale: [
-              [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
-              [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
-              [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
-          ],
+          autocolorscale: true,
           colorbar: {
               title: 'Percent Uninsured',
               thickness: 0.2
